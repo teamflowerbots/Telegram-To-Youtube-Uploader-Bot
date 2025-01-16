dcdde2aacf5983dee361347d48cbffb6fe76aec3
teamflowerbots <williamjackhacker1@gmail.com>
2025-01-16 18:02:03 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7649176510:AAHeWPvpk869PTVrVrgNX8hU3NETjLljjLo"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "YouTube_uploder_tf_bot"
 
-    API_ID = ""
+    API_ID = "22038177"
 
-    API_HASH = ""
+    API_HASH = "827c1a0d4058223737f7a254ec91eaaf"
 
     CLIENT_ID = ""
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5463387016]
 
     VIDEO_DESCRIPTION = ""
 
